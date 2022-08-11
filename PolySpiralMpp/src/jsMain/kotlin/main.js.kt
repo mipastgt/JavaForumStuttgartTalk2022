5c667411b223556cdd924eb67b3b4f678126e52e
mipastgt <mp@jugs.org>
2022-08-11 15:23:40 +0200

    Update main.js.kt

--- PolySpiralMpp/src/jsMain/kotlin/main.js.kt
+++ PolySpiralMpp/src/jsMain/kotlin/main.js.kt
@@ -1,5 +1,4 @@
 import androidx.compose.material.MaterialTheme
-import androidx.compose.ui.window.Window
 import de.mpmediasoft.polyspiral.gui.PolySpiralApp
 import org.jetbrains.skiko.wasm.onWasmReady
 
